4aa1b1e5861409542a17d0d3879e2093dd4449bc
davidhorseling <50203609+davidhorseling@users.noreply.github.com>
2020-06-04 17:56:07 +0200

    Umfragen inklusive Fragen können angelegt werden
    Als nächtes müssen wir Kurse mit Umfragen verknüpfen

--- creatorPage.php
+++ creatorPage.php
@@ -4,3 +4,5 @@ include_once 'includes.php';
 <a href="listCourses.php" title="Kursliste anzeigen und neue Kurse anlegen"> Liste aller Kurse anzeigen oder neue anlegen</a>
 <br>
 <a href="createStudents.php" title="Neue Studenten anlegen"> Neue Studenten anlegen </a>
+<br>
+<a href="declareAmountOfQuestions.php" title="Neue Umfrage erstellen"> Neue Umfrage erstellen </a>
---
+++ declareAmountOfQuestions.php
@@ -0,0 +1,10 @@
+<!--AUTHOR: David Horseling -->
+<?php
+include_once "includes.php";
+
+?>
+<form method="post" action="createQuestionnaire.php">
+  Anzahl Fragen der Umfrage <br>
+  <input type="Number" name="fname" min="1"> <br>
+  <input type="submit" name="Submit" value="Submit"><br>
+</form>
