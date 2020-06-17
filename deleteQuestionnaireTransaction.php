0cc90e6165c4530971d1cebb2a5f093851865490
Viktor Matussis <matusssisviktor@gmail.com>
2020-06-17 10:51:17 +0200

    Create deleteQuestionnaireTransaction.php

---
+++ deleteQuestionnaireTransaction.php
@@ -0,0 +1,24 @@
+<!--AUTHOR: Viktor Matussis -->
+<?php
+include_once 'includes.php';
+
+
+//Im Folgenden wird überprüft, dass der Primärschlüssel für die Frage übergeben wurde
+if (!isset($_POST["QUESTIONNAIRE_ID"])) {
+    echo "No Questionnnaire_ID";
+} else {
+    $QUESTIONNAIRE_ID = $_POST["QUESTIONNAIRE_ID"];
+}
+
+
+
+$sql = "DELETE FROM QUESTION WHERE  QUESTIONNAIRE_ID = ?";
+            $stmt = mysqli_prepare($connection, $sql);
+            mysqli_stmt_bind_param($stmt, 'ss', $QUESTIONNAIRE_ID, $Q_NUMBER);
+            if (!mysqli_stmt_execute($stmt)) {
+                echo "Etwas ist schief gelaufen.";
+            } else {
+				echo "Fragen wurden gelöscht.";
+			}
+}
+?>
