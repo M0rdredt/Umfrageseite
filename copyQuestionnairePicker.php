ae970d16d6dd0ad52b7083f240912b6b6c36ec55
Viktor Matussis <matusssisviktor@gmail.com>
2020-06-17 12:42:48 +0200

    Update copyQuestionnairePicker.php

--- copyQuestionnairePicker.php
+++ copyQuestionnairePicker.php
@@ -6,13 +6,13 @@ include_once 'includes.php';
 echo "<h1>Hier können Sie die Umfrage auswählen, die sie kopieren möchten</h1>";
 
 //Das HTML wird in diese Variable geschrieben um es am Ende über Echo auszugeben
-$formToCopyQuestionnaire =	"<form method=\"post\" action=\"copyQuestionnaire.php\">";
+$formToCopyQuestionnaire =	"<form method=\"post\" action=\"copyQuestionnaireTransaction.php\">";
 
 //nur die Umfragen anzeigen, die von diesem User erstellt wurden
 $USER = $_SESSION["User"];
 $USER_ID = $USER["USER_ID"];
 //siehe deleteQuestionsFromQuestionnaire
-$sql2 = "select NAME from QUESTIONNAIRE where USER_ID = ? AND QUESTIONNAIRE_ID NOT IN (SELECT QUESTIONNAIRE_ID FROM WORKS_WITH)";
+$sql2 = "select NAME from QUESTIONNAIRE where USER_ID = ?";
 $stmt = mysqli_prepare($connection, $sql2);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
@@ -37,7 +37,8 @@ $stmt = mysqli_prepare($connection, $sql2);
 
 			echo $formToCopyQuestionnaire;
 			?>
-
+      <h1> Hier bitte den Namen der neuen Umfrage einfügen </h1>
+      <input type="text" name="QuestionnaireName" required >
 			<br>
 			<br>
 			<input type="Submit" value="Diese Umfrage soll kopiert werden.">
