6cfa4975d3b573c0a9e1fcebb78d6e661f6b9969
Viktor Matussis <matusssisviktor@gmail.com>
2020-06-17 11:33:06 +0200

    Update copyQuestionnairePicker.php

--- copyQuestionnairePicker.php
+++ copyQuestionnairePicker.php
@@ -1,6 +1,6 @@
 <!--AUTHOR: Viktor Matussis -->
 <?php
-include_once "includes.php";
+include_once 'includes.php';
 
 
 echo "<h1>Hier können Sie die Umfrage auswählen, die sie kopieren möchten</h1>";
